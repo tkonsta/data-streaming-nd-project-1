ed0d01acb65ec695f1dda4efcc91797224e27f92
Thomas Konstantinides <14596668+tkonsta@users.noreply.github.com>
2023-02-24 14:52:10 +0100

    work on consumers (wip)

--- consumers/faust_stream.py
+++ consumers/faust_stream.py
@@ -3,6 +3,7 @@ import logging
 
 import faust
 
+from consumers.topic_config import TOPIC_BASE
 
 logger = logging.getLogger(__name__)
 
@@ -33,16 +34,16 @@ class TransformedStation(faust.Record):
 #   places it into a new topic with only the necessary information.
 app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
 # TODO: Define the input Kafka Topic. Hint: What topic did Kafka Connect output to?
-# topic = app.topic("TODO", value_type=Station)
+topic = app.topic(f"{TOPIC_BASE}.stations", value_type=Station)
 # TODO: Define the output Kafka Topic
-# out_topic = app.topic("TODO", partitions=1)
+out_topic = app.topic(f"{TOPIC_BASE}.stations-transformed", partitions=1)
 # TODO: Define a Faust Table
-#table = app.Table(
-#    # "TODO",
-#    # default=TODO,
-#    partitions=1,
-#    changelog_topic=out_topic,
-#)
+table = app.Table(
+   "stations-transformed",
+   default=int,
+   partitions=1,
+   changelog_topic=out_topic,
+)
 
 
 #
@@ -50,9 +51,23 @@ app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memor
 # TODO: Using Faust, transform input `Station` records into `TransformedStation` records. Note that
 # "line" is the color of the station. So if the `Station` record has the field `red` set to true,
 # then you would set the `line` of the `TransformedStation` record to the string `"red"`
-#
-#
+@app.agent(topic)
+async def stations_stream_processor(stations_stream):
+    async for station in stations_stream:
+        transformed = TransformedStation()
+        transformed.station_id = station.station_id
+        transformed.station_name = station.station_name
+        transformed.order = station.order
+        if station.red:
+            transformed.line = "red"
+        elif station.blue:
+            transformed.line = "blue"
+        elif station.green:
+            transformed.line = "green"
+        else:
+            raise Exception(f"No valid line has been given for station {station.station_id}")
 
+        table[transformed.station_id] = transformed
 
 if __name__ == "__main__":
     app.main()
---
+++ consumers/topic_config.py
@@ -0,0 +1,2 @@
+TOPIC_BASE = "com.udacity.dsnd.project1"
+"""Defines the base path for all topics. The part "dsnd" stands for 'Data Streaming Nanodegree'"""
